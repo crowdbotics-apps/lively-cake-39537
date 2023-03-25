df80b5539d4e1c9f4f49734b04f43783f8d3a032
Crowdbotics <team@crowdbotics.com>
2023-03-25 04:26:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-cake-39537",
+  url: "https://lively-cake-39537.botics.co",
+  api: "https://lively-cake-39537.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
